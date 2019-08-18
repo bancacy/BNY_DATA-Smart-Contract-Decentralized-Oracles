f81515de92a5ae75c6050e33cc70c9c6ba5f5472
AndyPuppyZ <44590470+AndyPuppyZ@users.noreply.github.com>
2019-08-18 21:25:48 +0300

    SignedSafeMath

--- BNY_DATA.sol
+++ BNY_DATA.sol
@@ -671,7 +671,37 @@ library SignedSafeMath {
         return c;
     }
 
-   
+    /**
+     * @dev Integer division of two signed integers truncating the quotient, reverts on division by zero.
+     */
+    function div(int256 a, int256 b) internal pure returns (int256) {
+        require(b != 0, "SignedSafeMath: division by zero");
+        require(!(b == -1 && a == INT256_MIN), "SignedSafeMath: division overflow");
+
+        int256 c = a / b;
+
+        return c;
+    }
+
+    /**
+     * @dev Subtracts two signed integers, reverts on overflow.
+     */
+    function sub(int256 a, int256 b) internal pure returns (int256) {
+        int256 c = a - b;
+        require((b >= 0 && c <= a) || (b < 0 && c > a), "SignedSafeMath: subtraction overflow");
+
+        return c;
+    }
+
+    /**
+     * @dev Adds two signed integers, reverts on overflow.
+     */
+    function add(int256 a, int256 b) internal pure returns (int256) {
+        int256 c = a + b;
+        require((b >= 0 && c >= a) || (b < 0 && c < a), "SignedSafeMath: addition overflow");
+
+        return c;
+    }
 }
 
 pragma solidity 0.4.24;
